db7806ef6280fe778efc1d3cec90ffe0c7cc55b1
Esgar Rodríguez <esgar.rodriguez765@gmail.com>
2022-07-28 14:01:55 -0500

    add first to screens

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-  }
+    gsap.registerPlugin(ScrollTrigger);
 
+    //Loop over all the sections and set animations
+    gsap.utils.toArray('section').forEach((section: any, i) => {
+      // Set the bg variable for the section
+      section.bg = section.querySelector('.bg');
+
+      // Give the backgrounds some random images
+      section.bg.style.backgroundImage = `url(https://picsum.photos/${innerWidth}/${innerHeight}?random=${i})`;
+
+      // Set the initial position for the background
+      section.bg.style.backgroundPosition = `50% ${-innerHeight / 2}px`;
+
+      // Do the parallax effect on each section
+      gsap.to(section.bg, {
+        backgroundPosition: `50% ${innerHeight / 2}px`,
+        ease: 'none', // Don't apply any easing function.
+        scrollTrigger: {
+          // Trigger the animation as soon as the section comes into view
+          trigger: section,
+          // Animate on scroll/scrub
+          scrub: true,
+        },
+      });
+    });
+  }
 }
