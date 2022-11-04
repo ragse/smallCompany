5e46d0dbca67afe944afb7630a137436658ba8a5
Esgar Rodríguez <esgar.rodriguez765@gmail.com>
2022-11-04 15:53:20 -0600

    progress

---
+++ src/app/constants/home.constants.ts
@@ -0,0 +1,92 @@
+export const textUser = {
+  seccionOne: {
+    title: 'Mkt Digital&Desarrollo Web',
+    content: '<p>Linkstart más que una Agencia Digital, será tu Socio Estratégico para que puedas ir más allá y revolucionar tu negocio en la era digital con nuestras estrategias exclusivas.</p>',
+    buttons: [
+      {
+        text: 'MÁS INFORMACIÓN',
+        redirect: ''
+      },
+      {
+        text: 'CONTACTO',
+        redirect: ''
+      }
+    ]
+  },
+  seccionTwo: {
+    partOne: {
+      title: 'Campañas Publicitarias Meta Ads',
+      subTitle: 'Facebook&Instagram',
+      content: '<ul><li><span>Campañas de Clientes Potenciales</span></li><li><span>Campañas de Generación de Ventas</span></li><li><span>Segmentación Estratégica</span></li><li><span>Diseño de Anuncios y Copys Persuasivos</span></li></ul>',
+      button: {
+        text: 'CONOCER PLANES',
+        redirect: ''
+      }
+    },
+    partTwo: {
+      title: 'Campañas Publicitarias Google',
+      content: '<ul class="list-unstyled"><li><span>Campañas de Anuncios de Texto</span></li><li><span>Campañas de Display</span></li><li><span>Campañas de Video</span></li></ul>',
+      content2: '<ul class="list-unstyled"><li><span>Campañas de Shopping</span></li><li><span>Campañas Discovery</span></li><li><span>Campañas de Rmkt</span></li></ul>',
+      button: {
+        text: 'COTIZAR',
+        redirect: ''
+      }
+    }
+  },
+  seccionThree: {
+    partOne: {
+      title: 'Desarrollo Web',
+      content: '<ul><li>Landing Page</li><li>Sitio Web</li><li>Optimización SEO</li><li>E-commerce</li></ul>',
+      component2: 'Realizamos esa página web que tanto has soñado y que cambiará tu negocio para siempre.',
+    },
+    partTwo: {
+      title: 'E-commerce',
+      content:'Desarrollamos tu tienda virtual para que vendas las 24 horas, en cualquier lado y crezcas tal como siempre lo has deseado, aumentando tus ventas y generación de clientes.',
+      button: {
+        text: 'VER PRECIOS',
+        redirect: ''
+      }
+    }
+  },
+  seccionFour: {
+    title: 'Deja de Perder Ventas',
+    subTitle: '¡Nosotros te ayudamos hoy mismo!',
+    cards: [
+      {
+        title: '45%',
+        content: 'De los negocios venden sus productos en internet actualmente.'
+      },
+      {
+        title: '71%',
+        content: 'De las personas en México usan redes sociales todos los días.'
+      },
+      {
+        title: '86%',
+        content: 'De los compradores buscan un producto o servicio en internet para comprarlo.'
+      }
+    ]
+  },
+  seccionFive: {
+    seccionOne: {
+      title: 'Social Media Manejo de Redes Sociales',
+      subTitle: 'Manejo de Redes Sociales',
+      content: '<ul><li><span>Planeación Estratégica</span></li><li><span>Diseño de Identidad de Marca</span></li><li><span>Creación de Contenido Multimedia</span></li><li><span>Medición, Análisis y Optimización</span></li></ul>',
+      button: {
+        text: 'CONOCER PLANES',
+        redirect: ''
+      }
+    }
+  },
+  seccionSix: {
+    title: 'Nuestros Objetivos'
+  },
+  seccionSeven: {
+    title: 'Contactanos Ahora',
+    content: 'Linkstart Es El Mejor Socio Estratégico'
+  }
+}
+
+
+
+
+
